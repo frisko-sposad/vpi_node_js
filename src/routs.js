86aa199d08519cfef689013699076582e2876e2c
frisko-sposad <safiko@ya.ru>
2024-11-25 00:30:52 +0300

    cors

--- src/routs.js
+++ src/routs.js
@@ -20,7 +20,6 @@ router.get('/page1', function (req, res) {
 router.get('/users', function (req, res) {
   res.header('Access-Control-Allow-Origin', '*');
   // res.send('users');
-  res.header('Access-Control-Allow-Origin', '*');
   pool.query('SELECT * FROM users', function (err, results) {
     if (err) console.log(err);
     console.log(results);
@@ -75,6 +74,7 @@ router.post('/add_user', async function (req, res) {
 router.get('/user/:userId', function (req, res) {
   // res.send('users');
   res.header('Access-Control-Allow-Origin', '*');
+
   console.log(req.params);
   pool.query(
     `SELECT hero_id, hero_name, hero_title, hero_name, login, games_id, locations.locations_name,  locations.locations_id, concat_view.concat_loc FROM heroes JOIN (houses, users, locations) ON (heroes.hero_house = houses.house_id  AND heroes.hero_owner = users.user_id AND heroes.hero_location = locations.locations_id) JOIN (SELECT  path_graph_location_id, GROUP_CONCAT(locations.locations_name) as concat_loc FROM locations JOIN (path_graph) ON ( locations.locations_id= path_graph_location_near)  GROUP BY path_graph_location_id) as concat_view ON locations.locations_id=path_graph_location_id WHERE heroes.hero_owner = '${req.params.userId}'`,
