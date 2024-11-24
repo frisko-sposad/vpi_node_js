e1bdbc9ace70f24f772821fbaafae5b6db558860
frisko-sposad <safiko@ya.ru>
2024-11-25 00:47:39 +0300

    up cors

--- src/routs.js
+++ src/routs.js
@@ -62,7 +62,18 @@ router.delete('/delete_users/:userId', function (req, res) {
 
 // добавить юзера
 router.post('/add_user', async function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
   const { user_id, login, pass, role, info } = await req.body;
   console.log(user_id, login, pass, role, info);
   pool.query(
