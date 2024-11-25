4397799b67416727d9cb766a7f3d682c82f9e3a8
frisko-sposad <safiko@ya.ru>
2024-11-25 09:54:23 +0300

    cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express, { json } from 'express';
 import router from './src/routs.js';
+const cors = require('cors');
 var app = express();
 
 const PORT = 5000;
-
+app.use(cors());
 app.use(json());
 
 app.use('/', router);
